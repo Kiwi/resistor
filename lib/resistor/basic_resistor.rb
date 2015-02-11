9e72a01d953d8a4799227157ff188e264b637c8f
seinosuke <seinosuke.3606@gmail.com>
2015-02-11 22:25:00 +0900

    Resistor.newでohmかcodeを指定しない仕様に変更

--- lib/resistor/basic_resistor.rb
+++ lib/resistor/basic_resistor.rb
@@ -1,31 +1,33 @@
 module Resistor
   class BasicResistor
 
-    attr_reader :ohm, :code, :error_range
+    attr_reader :ohm, :code, :tolerance
 
     # Initializes a new BasicResistor object.
     # Both the ohm and code parameter are optional,
     # but at least one of them must be supplied.
     #
-    # @option options [Integer, Float] :ohm(nil) resistance value
-    # @option options [Array<Symbol>, Array<String>] :code(nil) color code
-    # @option options [Integer, Float] :error_range(5.0)
+    # @overload initialize(arg, options = {})
+    #   @param arg [Integer, Float] A resistance value.
+    # @overload initialize(arg, options = {})
+    #   @param arg [Array<Symbol>, Array<String>] A color code.
+    # @option options [Integer, Float] :tolerance(5.0)
     # @raise [ArgumentError] The ohm or code parameter must be supplied.
     #   Error raised if neither parameter is supplied.
     # @return [Resistor::BasicResistor]
-    # def initialize(ohm:nil, code:nil, error_range: 5.0)
-    def initialize(options = {})
-      options[:error_range] ||= 5.0
-      opt = {:error_range => options[:error_range]}
-      if options[:ohm]
-        @ohm = options[:ohm].to_f
+    def initialize(arg, options = {})
+      options[:tolerance] ||= 5.0
+      opt = {:tolerance => options[:tolerance]}
+
+      case arg
+      when Integer, Float
+        @ohm = arg.to_f
         @code = Resistor::ColorCode.encode(@ohm, opt)
-        @error_range = options[:error_range]
-      elsif options[:code]
-        raise ArgumentError unless options[:code].is_a? Array
-        @code = options[:code].map(&:to_sym)
+        @tolerance = options[:tolerance]
+      when Array
+        @code = arg.map(&:to_sym)
         @ohm = Resistor::ColorCode.decode(@code)
-        @error_range = Resistor::ColorCode::ERROR_RANGE[@code[3].to_sym]
+        @tolerance = Resistor::ColorCode::TOLERANCE[@code[3].to_sym]
       else
         raise ArgumentError
       end
@@ -42,13 +44,13 @@ module Resistor
 
     # Set a color code.
     # When the color code is changed, the resistance value is also changed,
-    # and the error range is also changed.
+    # and the tolerance is also changed.
     #
     # @param code [Array<Symbol>, Array<String>] color code
     def code=(code)
       @code = code.map(&:to_sym)
       @ohm = Resistor::ColorCode.decode(@code)
-      @error_range = Resistor::ColorCode::ERROR_RANGE[@code[3].to_sym]
+      @tolerance = Resistor::ColorCode::TOLERANCE[@code[3].to_sym]
     end
 
     # Calculates a series combined resistance value.
@@ -71,8 +73,8 @@ module Resistor
 
     # @return [Boolean] Whether or not the resistance value is the E12 series.
     def e12?
-      num0 = Resistor::ColorCode::NUM[@code[0]]
-      num1 = Resistor::ColorCode::NUM[@code[1]]
+      num0 = Resistor::ColorCode::DIGIT[@code[0]]
+      num1 = Resistor::ColorCode::DIGIT[@code[1]]
       Resistor::ColorCode::E12_SERIES.each do |key, val|
         if num0 == key
           return true if val.any? { |e| e == num1 }
@@ -83,8 +85,8 @@ module Resistor
 
     # @return [Boolean] Whether or not the resistance value is the E24 series.
     def e24?
-      num0 = Resistor::ColorCode::NUM[@code[0]]
-      num1 = Resistor::ColorCode::NUM[@code[1]]
+      num0 = Resistor::ColorCode::DIGIT[@code[0]]
+      num1 = Resistor::ColorCode::DIGIT[@code[1]]
       Resistor::ColorCode::E24_SERIES.each do |key, val|
         if num0 == key
           return true if val.any? { |e| e == num1 }
--- spec/basic_resistor_spec.rb
+++ spec/basic_resistor_spec.rb
@@ -5,68 +5,65 @@ require_relative "spec_helper"
 describe Resistor::BasicResistor do
 
   describe '::new' do
-    let(:resistor) { Resistor.new(ohm: ohm, code: code) }
+    let(:resistor) { Resistor.new(arg) }
 
     context '抵抗値を指定した場合' do
-      let(:ohm) { 4700 }
-      let(:code) { nil }
+      let(:arg) { 4700 }
 
       it { expect(resistor.ohm).to eq 4700.0 }
       it { expect(resistor.code).to eq [:yellow, :purple, :red, :gold] }
-      it { expect(resistor.error_range).to eq 5.0 }
+      it { expect(resistor.tolerance).to eq 5.0 }
     end
 
     context '抵抗値を指定した場合' do
-      let(:ohm) { nil }
-      let(:code) { ['brown', 'black', 'blue', 'silver'] }
+      let(:arg) { ['brown', 'black', 'blue', 'silver'] }
 
       it { expect(resistor.ohm).to eq 10_000_000 }
       it { expect(resistor.code).to eq [:brown, :black, :blue, :silver] }
-      it { expect(resistor.error_range).to eq 10.0 }
+      it { expect(resistor.tolerance).to eq 10.0 }
     end
 
     context 'どちらも指定しなかった場合' do
-      let(:ohm) { nil }
-      let(:code) { nil }
+      let(:arg) { nil }
 
       it { expect { is_expected }.to raise_error(ArgumentError) }
     end
 
     context '許容差を指定した場合' do
-      resistor = Resistor.new(ohm: 100, error_range: 0.1)
+      resistor = Resistor.new(100, tolerance: 0.1)
       it { expect(resistor.code[3]).to eq :purple }
-      it { expect(resistor.error_range).to eq 0.1 }
+      it { expect(resistor.tolerance).to eq 0.1 }
     end
   end
 
   describe '#+, #/' do
     context '直列接続の合成抵抗' do
-      r1 = Resistor.new(ohm: 100)
-      r2 = Resistor.new(ohm: 200)
-      r3 = Resistor.new(ohm: 300)
+      r1 = Resistor.new(100)
+      r2 = Resistor.new(200)
+      r3 = Resistor.new(300)
 
       it { expect((r1 + r2 + r3).ohm).to eq 600.0 }
     end
 
     context '並列接続の合成抵抗' do
-      r1 = Resistor.new(ohm: 30)
-      r2 = Resistor.new(ohm: 15)
-      r3 = Resistor.new(ohm: 10)
+      r1 = Resistor.new(30)
+      r2 = Resistor.new(15)
+      r3 = Resistor.new(10)
 
       it { expect((r1 / r2 / r3).ohm).to eq 5.0 }
     end
 
     context '並列と直列を合わせた合成抵抗' do
-      r1 = Resistor.new(ohm: 20)
-      r2 = Resistor.new(ohm: 30)
-      r3 = Resistor.new(ohm: 4)
-      r4 = Resistor.new(ohm: 8)
+      r1 = Resistor.new(20)
+      r2 = Resistor.new(30)
+      r3 = Resistor.new(4)
+      r4 = Resistor.new(8)
 
       it { expect(((r1 / r2) + r3 + (r4 / r4)).ohm).to eq 20.0 }
     end
 
     context '#+のエイリアスメソッドを使用した場合' do
-      r1 = Resistor.new(ohm: 100)
+      r1 = Resistor.new(100)
       original_name = (r1 + r1 + r1).ohm
       new_name = (r1 - r1 - r1).ohm
 
@@ -74,7 +71,7 @@ describe Resistor::BasicResistor do
     end
 
     context '#/のエイリアスメソッドを使用した場合' do
-      r1 = Resistor.new(ohm: 100)
+      r1 = Resistor.new(100)
       original_name = (r1 / r1 / r1).ohm
       new_name = (r1 | r1 | r1).ohm
 
@@ -84,7 +81,7 @@ describe Resistor::BasicResistor do
 
   describe '#ohm=, #code=' do
     context '抵抗値を変更する場合' do
-      resistor = Resistor.new(ohm: 100)
+      resistor = Resistor.new(100)
       resistor.ohm = 4.7
 
       it { expect(resistor.ohm).to eq 4.7 }
@@ -92,44 +89,44 @@ describe Resistor::BasicResistor do
     end
 
     context 'カラーコードを変更する場合' do
-      resistor = Resistor.new(ohm: 100)
+      resistor = Resistor.new(100)
       resistor.code = ['green', 'brown', 'silver', 'brown']
 
       it { expect(resistor.ohm).to eq 0.51 }
       it { expect(resistor.code).to eq [:green, :brown, :silver, :brown] }
-      it { expect(resistor.error_range).to eq 1.0 }
+      it { expect(resistor.tolerance).to eq 1.0 }
     end
   end
 
   describe '#e12?' do
     context 'E12系列である場合' do
-      it { expect(Resistor.new(ohm: 0.12).e12?).to eq true }
-      it { expect(Resistor.new(ohm: 3.3).e12?).to eq true }
-      it { expect(Resistor.new(ohm: 47).e12?).to eq true }
-      it { expect(Resistor.new(ohm: 82_000_000).e12?).to eq true }
+      it { expect(Resistor.new(0.12).e12?).to eq true }
+      it { expect(Resistor.new(3.3).e12?).to eq true }
+      it { expect(Resistor.new(47).e12?).to eq true }
+      it { expect(Resistor.new(82_000_000).e12?).to eq true }
     end
 
     context 'E12系列でない場合' do
-      it { expect(Resistor.new(ohm: 0.13).e12?).to eq false }
-      it { expect(Resistor.new(ohm: 3.8).e12?).to eq false }
-      it { expect(Resistor.new(ohm: 55).e12?).to eq false }
-      it { expect(Resistor.new(ohm: 70_000_000).e12?).to eq false }
+      it { expect(Resistor.new(0.13).e12?).to eq false }
+      it { expect(Resistor.new(3.8).e12?).to eq false }
+      it { expect(Resistor.new(55).e12?).to eq false }
+      it { expect(Resistor.new(70_000_000).e12?).to eq false }
     end
   end
 
   describe '#e24?' do
     context 'E24系列である場合' do
-      it { expect(Resistor.new(ohm: 0.1).e24?).to eq true }
-      it { expect(Resistor.new(ohm: 3).e24?).to eq true }
-      it { expect(Resistor.new(ohm: 47_000).e24?).to eq true }
-      it { expect(Resistor.new(ohm: 10_000_000).e24?).to eq true }
+      it { expect(Resistor.new(0.1).e24?).to eq true }
+      it { expect(Resistor.new(3).e24?).to eq true }
+      it { expect(Resistor.new(47_000).e24?).to eq true }
+      it { expect(Resistor.new(10_000_000).e24?).to eq true }
     end
 
     context 'E24系列でない場合' do
-      it { expect(Resistor.new(ohm: 0.92).e24?).to eq false }
-      it { expect(Resistor.new(ohm: 8.3).e24?).to eq false }
-      it { expect(Resistor.new(ohm: 23_000).e24?).to eq false }
-      it { expect(Resistor.new(ohm: 52_000_000).e24?).to eq false }
+      it { expect(Resistor.new(0.92).e24?).to eq false }
+      it { expect(Resistor.new(8.3).e24?).to eq false }
+      it { expect(Resistor.new(23_000).e24?).to eq false }
+      it { expect(Resistor.new(52_000_000).e24?).to eq false }
     end
   end
 end
