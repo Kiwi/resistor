ea380e464459fe72ef54356879ed56a9f3196308
seinosuke <seinosuke.3606@gmail.com>
2015-02-14 21:30:36 +0900

    5本帯に関するテストを追加

--- spec/basic_resistor_spec.rb
+++ spec/basic_resistor_spec.rb
@@ -9,7 +9,6 @@ describe Resistor::BasicResistor do
 
     context '抵抗値を指定した場合' do
       let(:arg) { 4700 }
-
       it { expect(resistor.ohm).to eq 4700.0 }
       it { expect(resistor.code).to eq [:yellow, :purple, :red, :gold] }
       it { expect(resistor.tolerance).to eq 5.0 }
@@ -17,7 +16,6 @@ describe Resistor::BasicResistor do
 
     context '抵抗値を指定した場合' do
       let(:arg) { ['brown', 'black', 'blue', 'silver'] }
-
       it { expect(resistor.ohm).to eq 10_000_000 }
       it { expect(resistor.code).to eq [:brown, :black, :blue, :silver] }
       it { expect(resistor.tolerance).to eq 10.0 }
@@ -25,7 +23,6 @@ describe Resistor::BasicResistor do
 
     context 'どちらも指定しなかった場合' do
       let(:arg) { nil }
-
       it { expect { is_expected }.to raise_error(ArgumentError) }
     end
 
@@ -34,6 +31,46 @@ describe Resistor::BasicResistor do
       it { expect(resistor.code[3]).to eq :purple }
       it { expect(resistor.tolerance).to eq 0.1 }
     end
+
+    context 'オプションで5本帯を指定した場合' do
+      # 許容差は変更されない
+      resistor = Resistor.new(6.04, band_number: 5)
+      it { expect(resistor.code).to eq [:blue, :black, :yellow, :silver, :gold] }
+      it { expect(resistor.tolerance).to eq 5.0 }
+    end
+
+    context 'メソッドで5本帯を指定した場合' do
+      # 許容差まで変更される
+      Resistor::Options.set_band_number 5
+      resistor = Resistor.new(123)
+      it { expect(resistor.code).to eq [:brown, :red, :orange, :black, :brown] }
+      it { expect(resistor.tolerance).to eq 1.0 }
+      Resistor::Options.set_band_number 4
+    end
+
+    context 'メソッドで5本帯と許容差を指定した場合' do
+      # 許容差まで指定できる
+      Resistor::Options.defaults do |d|
+        d[:band_number] = 5
+        d[:tolerance] = 0.5
+      end
+      resistor = Resistor.new(49900)
+      it { expect(resistor.code).to eq [:yellow, :white, :white, :red, :green] }
+      it { expect(resistor.tolerance).to eq 0.5 }
+      Resistor::Options.set_band_number 4
+    end
+
+    context 'メソッドで5本帯を指定してさらにオプションを渡した場合' do
+      # オプションで渡されたものに上書きされる
+      Resistor::Options.defaults do |d|
+        d[:band_number] = 5
+        d[:tolerance] = 0.5
+      end
+      resistor = Resistor.new(84.5, tolerance:0.1)
+      it { expect(resistor.code).to eq [:gray, :yellow, :green, :gold, :purple] }
+      it { expect(resistor.tolerance).to eq 0.1 }
+      Resistor::Options.set_band_number 4
+    end
   end
 
   describe '#+, #/' do
@@ -41,7 +78,6 @@ describe Resistor::BasicResistor do
       r1 = Resistor.new(100)
       r2 = Resistor.new(200)
       r3 = Resistor.new(300)
-
       it { expect((r1 + r2 + r3).ohm).to eq 600.0 }
     end
 
@@ -49,7 +85,6 @@ describe Resistor::BasicResistor do
       r1 = Resistor.new(30)
       r2 = Resistor.new(15)
       r3 = Resistor.new(10)
-
       it { expect((r1 / r2 / r3).ohm).to eq 5.0 }
     end
 
@@ -58,7 +93,6 @@ describe Resistor::BasicResistor do
       r2 = Resistor.new(30)
       r3 = Resistor.new(4)
       r4 = Resistor.new(8)
-
       it { expect(((r1 / r2) + r3 + (r4 / r4)).ohm).to eq 20.0 }
     end
 
@@ -66,7 +100,6 @@ describe Resistor::BasicResistor do
       r1 = Resistor.new(100)
       original_name = (r1 + r1 + r1).ohm
       new_name = (r1 - r1 - r1).ohm
-
       it { expect(original_name == new_name).to be_truthy }
     end
 
@@ -74,7 +107,6 @@ describe Resistor::BasicResistor do
       r1 = Resistor.new(100)
       original_name = (r1 / r1 / r1).ohm
       new_name = (r1 | r1 | r1).ohm
-
       it { expect(original_name == new_name).to be_truthy }
     end
   end
@@ -83,7 +115,6 @@ describe Resistor::BasicResistor do
     context '抵抗値を変更する場合' do
       resistor = Resistor.new(100)
       resistor.ohm = 4.7
-
       it { expect(resistor.ohm).to eq 4.7 }
       it { expect(resistor.code).to eq [:yellow, :purple, :gold, :gold] }
     end
@@ -91,7 +122,6 @@ describe Resistor::BasicResistor do
     context 'カラーコードを変更する場合' do
       resistor = Resistor.new(100)
       resistor.code = ['green', 'brown', 'silver', 'brown']
-
       it { expect(resistor.ohm).to eq 0.51 }
       it { expect(resistor.code).to eq [:green, :brown, :silver, :brown] }
       it { expect(resistor.tolerance).to eq 1.0 }
--- spec/color_code_spec.rb
+++ spec/color_code_spec.rb
@@ -5,65 +5,128 @@ require_relative "spec_helper"
 describe Resistor::ColorCode do
 
   describe '::encode' do
-    subject { Resistor::ColorCode.encode(ohm) }
+    subject { Resistor::ColorCode.encode(ohm, options) }
 
     context '0Ωの場合' do
       let(:ohm) { 0 }
+      let(:options) { {} }
       it { is_expected.to eq [:black] }
     end
 
     context '0.1Ω以上1Ωより小さい場合' do
       let(:ohm) { 0.24 }
+      let(:options) { {} }
       it { is_expected.to eq [:red, :yellow, :silver, :gold] }
     end
 
     context '1Ω以上10Ωより小さい場合' do
       let(:ohm) { 2 }
+      let(:options) { {} }
       it { is_expected.to eq [:red, :black, :gold, :gold] }
     end
 
     context '10Ω以上の場合' do
       let(:ohm) { 3300 }
+      let(:options) { {} }
       it { is_expected.to eq [:orange, :orange, :red, :gold] }
     end
 
     context '0Ωより小さい場合' do
       let(:ohm) { -10 }
+      let(:options) { {} }
       it { expect { is_expected }.to raise_error(ArgumentError) }
     end
 
     context '0.1Ωより小さい場合' do
       let(:ohm) { 0.01 }
+      let(:options) { {} }
       it { expect { is_expected }.to raise_error(ArgumentError) }
+    end
+
+    context 'オプションで5本帯を指定した場合' do
+      # 許容差は変更されない
+      let(:ohm) { 4320 }
+      let(:options) { {:band_number => 5} }
+      it { is_expected.to eq [:yellow, :orange, :red, :brown, :gold] }
+    end
+
+    context 'メソッドで5本帯を指定した場合' do
+      # 許容差まで変更される
+      Resistor::Options.set_band_number 5
+      code = Resistor::ColorCode.encode(511)
+      it { expect(code).to eq [:green, :brown, :brown, :black, :brown] }
+      Resistor::Options.set_band_number 4
+    end
+
+    context 'メソッドで5本帯と許容差を指定した場合' do
+      # 許容差まで指定できる
+      Resistor::Options.defaults do |d|
+        d[:band_number] = 5
+        d[:tolerance] = 0.5
+      end
+      code = Resistor::ColorCode.encode(3.92)
+      it { expect(code).to eq [:orange, :white, :red, :silver, :green] }
+      Resistor::Options.set_band_number 4
+    end
+
+    context 'メソッドで5本帯を指定してさらにオプションを渡した場合' do
+      # オプションで渡されたものに上書きされる
+      Resistor::Options.defaults do |d|
+        d[:band_number] = 5
+        d[:tolerance] = 0.5
+      end
+      code = Resistor::ColorCode.encode(20.5, tolerance:0.1)
+      it { expect(code).to eq [:red, :black, :green, :gold, :purple] }
+      Resistor::Options.set_band_number 4
     end
   end
 
   describe '::decode' do
-    subject { Resistor::ColorCode.decode(code) }
+    subject { Resistor::ColorCode.decode(code, options) }
 
     context '0Ωの場合' do
       let(:code) { [:black] }
+      let(:options) { {} }
       it { is_expected.to eq 0 }
     end
 
     context '0.1Ω以上1Ωより小さい場合' do
       let(:code) { [:blue, :red, :silver, :gold] }
+      let(:options) { {} }
       it { is_expected.to eq 0.62 }
     end
 
     context '1Ω以上10Ωより小さい場合' do
       let(:code) { [:white, :brown, :gold, :gold] }
+      let(:options) { {} }
       it { is_expected.to eq 9.1 }
     end
 
     context '10Ω以上の場合' do
       let(:code) { [:brown, :black, :blue, :gold] }
+      let(:options) { {} }
       it { is_expected.to eq 10_000_000 }
     end
 
     context '1本目に黒が指定された場合' do
       let(:code) { [:black, :blue, :blue, :gold] }
+      let(:options) { {} }
       it { expect { is_expected }.to raise_error(ArgumentError) }
+    end
+
+    context 'オプションで5本帯を指定した場合' do
+      # 許容差は変更されない
+      let(:code) { [:yellow, :orange, :red, :brown, :gold] }
+      let(:options) { {:band_number => 5} }
+      it { is_expected.to eq 4320.0 }
+    end
+
+    context 'メソッドで5本帯を指定した場合' do
+      # 許容差まで変更される
+      Resistor::Options.set_band_number 5
+      ohm = Resistor::ColorCode.decode([:green, :brown, :brown, :black, :brown])
+      it { expect(ohm).to eq 511.0 }
+      Resistor::Options.set_band_number 4
     end
   end
 end
